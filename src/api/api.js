1fa54ff525d993b53f653e9f2cefc47bb704428c
Diago10 <diago.mendonca.10@gmail.com>
2025-02-04 23:23:32 +0530

    api changes .2

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const API_URL = '/api/Uw5CrX';
+const API_URL = 'https://api.jsonserve.com/Uw5CrX';
 
 export const fetchQuizData = async () => {
   try {
-    const response = await axios.get(API_URL); // Match this with the proxy setup
+    const response = await axios.get(API_URL);
     return response.data.questions;
   } catch (error) {
     console.error('Error fetching quiz data:', error.message);
     throw new Error('Failed to fetch quiz data');
   }
 };
+
