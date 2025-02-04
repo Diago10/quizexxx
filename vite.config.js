643e35da9e2baec935f8c64848c1a1a89b39453c
Diago10 <diago.mendonca.10@gmail.com>
2025-02-04 23:04:59 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  build: {
-    rollupOptions: {
-      external: ['axios']
-    }
-  },
   server: {
     proxy: {
       '/api': {
